4f71cb61a60d81a335c76ec6de65b04581027d1f
anarghya <anarghya@buffalo.edu>
2019-06-16 05:29:30 -0400

    :art: updated settings.py

--- django_graphql_movies/django_graphql_movies/settings.py
+++ django_graphql_movies/django_graphql_movies/settings.py
@@ -85,23 +85,35 @@ WSGI_APPLICATION = 'django_graphql_movies.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'NAME': 'userData',
-        'USER': 'anarghya'
+# [START db_setup]
+
+if os.getenv('GAE_APPLICATION', None):
+    # Running on production App Engine, so connect to Google Cloud SQL using
+    # the unix socket at /cloudsql/<your-cloudsql-connection string>
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '/cloudsql/codebuffalohack:us-central1:graph-table',
+            'USER': 'anarghya',
+            'NAME': 'userData',
+        }
     }
-}
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'HOST': '/cloudsql/codebuffalohack:us-central1:graph-table',
-#         'USER': 'anarghya',
-#         'NAME': 'userData',
-#     }
-# }
+else:
+    # Running locally so connect to either a local MySQL instance or connect
+    # to Cloud SQL via the proxy.  To start the proxy via command line:
+    #    $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
+    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            'NAME': 'userData',
+            'USER': 'anarghya'
+        }
+    }
+# [END db_setup]
+
 
 
 # Password validation
