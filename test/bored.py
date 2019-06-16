7e0492bf4c3b9489c03cd0fbe645d862051fb579
anarghya <anarghya@buffalo.edu>
2019-06-16 07:28:04 -0400

    :heavy_minus_sign: removed unused files

--- test/bored.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def get_activity(categories):
-    activities = []
-    for category in categories:
-        activities.append((requests.get("http://www.boredapi.com/api/activity", params={"type": category})).json()['activity'])
-    return activities
--- test/run.sh
+++
@@ -1,3 +0,0 @@
-python3 aritra.py
-python3 convert_pandas.py
-python3 testsurprise.py
