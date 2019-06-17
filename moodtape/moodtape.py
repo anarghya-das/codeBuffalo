8e8b4081238305699d2d52a51a42dfd8f0cd5929
Moulid Ahmed <moulidah@buffalo.edu>
2019-06-17 19:21:13 -0400

    Update moodtape.py

--- moodtape/moodtape.py
+++ moodtape/moodtape.py
@@ -20,8 +20,7 @@ import random
 #         - API: Add Tracks to a Playlist
 #         - Scope: playlist-modify-public
 
-client_id = "9a6e7be5e69a43f4bfbf7834ca8dd6c1"
-client_secret = "fd867b2e103d4f90bb456b57107f75e9"
+
 redirect_uri = "https://www.youtube.com/"
 
 
