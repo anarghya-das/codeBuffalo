233da71d31371f629035d53286ed7c9d3d38f046
Moulid Ahmed <moulidah@buffalo.edu>
2019-06-17 19:17:53 -0400

    Update spotify.sh

--- src/spotify.sh
+++ src/spotify.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-export SPOTIPY_CLIENT_ID='9a6e7be5e69a43f4bfbf7834ca8dd6c1'
-export SPOTIPY_CLIENT_SECRET='fd867b2e103d4f90bb456b57107f75e9'
 export SPOTIPY_REDIRECT_URI='https://www.youtube.com/'
